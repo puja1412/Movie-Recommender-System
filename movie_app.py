47226fdab21642b7a53cc7000cb976f7d6476162
Pooja Pathak <89625636+puja1412@users.noreply.github.com>
2022-10-06 15:43:09 +0530

    Add files via upload

---
+++ movie_app.py
@@ -0,0 +1,108 @@
+# import libraries
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+#fetch poster
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=4718c03e434fb3b233019dc4ae6cf040&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data =data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch the movie poster
+        recommended_movies_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies,recommended_movies_posters
+
+
+
+
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.subheader('Spotmovies   presents')
+#title
+st.title('Movie Recommender System')
+st.write('It will predict what movies  you will like based on the attributes present in previously'
+         ' liked movies. This Recommendation systems makes the selection process easier for you.')
+
+selected_movie_name = st.selectbox(
+  "Type or select a movie from the dropdown",
+  movies['title'].values
+ )
+
+if st.button('Show_Recommendation'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+
+
+
+
+def add_bg_from_url():
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80");
+
+   ");
+   ");
+
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         [data-testid="stHeader"] {{
+         background-color: rgba(0,0,0,0);
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+
+
+add_bg_from_url()
+
+# primaryColor="#F63366"
+# backgroundColor="#FFFFFF"
+# secondaryBackgroundColor="#F0F2F6"
+textColor = "#262730"
+font = "Serif"
